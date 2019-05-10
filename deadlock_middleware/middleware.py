ad38cbbdd007fad5804ad1c0d708d5e0fc7d4866
Carson Crane <carson.crane@esharesinc.com>
2019-05-09 19:57:04 -0700

    convert to new style middleware

--- deadlock_middleware/middleware.py
+++ deadlock_middleware/middleware.py
@@ -2,32 +2,38 @@ import logging
 
 from django.conf import settings
 from django.db import OperationalError
-from django.utils.deprecation import MiddlewareMixin
 
 logger = logging.getLogger(__name__)
 
 
-class DeadlockRetryMiddleware(MiddlewareMixin):
+class DeadlockRetryMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        request.deadlock_retry_attempt = 1
+
+        return self.get_response(request)
+
     def process_view(self, request, view_func, view_args, view_kwargs):
         non_atomic_requests = getattr(view_func, "_non_atomic_requests", set())
 
-        if non_atomic_requests:
-            return view_func(request, *view_args, **view_kwargs)
+        request.disable_deadlock_retry = bool(non_atomic_requests)
 
-        attempt = 0
+    def process_exception(self, request, exception):
+        if request.disable_deadlock_retry:
+            return None
 
-        retry_attempts = getattr(settings, "DEADLOCK_RETRY_ATTEMPTS", 2)
+        if not isinstance(exception, OperationalError):
+            return None
 
-        while attempt < retry_attempts:
-            try:
-                return view_func(request, *view_args, **view_kwargs)
-            except OperationalError as e:
-                is_deadlock = e.args[0].startswith("deadlock detected")
+        if not exception.args[0].startswith("deadlock detected"):
+            return None
 
-                if not is_deadlock:
-                    raise e
+        total_retry_attempts = getattr(settings, "DEADLOCK_RETRY_ATTEMPTS", 2)
+        if request.deadlock_retry_attempt < total_retry_attempts:
+            logger.warning('deadlock detected at {} - retrying'.format(request.path))
 
-                attempt += 1
-                logger.warning("deadlock detected - retrying request")
+            request.deadlock_retry_attempt += 1
 
-        return view_func(request, *view_args, **view_kwargs)
+            return self.get_response(request)
